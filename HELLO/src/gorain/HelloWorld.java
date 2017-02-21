44b67f40bd011f9c3f34aa588801cbea8c9bf133
Snehan <334752052@C-3ZWJYR1.tdsb.on.ca>
2017-02-21 14:51:57 -0500

    Pushing again

--- HELLO/src/gorain/HelloWorld.java
+++ HELLO/src/gorain/HelloWorld.java
@@ -1,4 +1,7 @@
 package gorain;
+
+import java.util.Scanner;
+
 /**
  * Hello Word
  * Program
@@ -6,10 +9,11 @@ package gorain;
  * February 21
  */
 public class HelloWorld {
-static Scanner 
+static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Hello, World!!!");
-		
+		String name = scan.nextLine();
+		System.out.println("Hi "+ name);
 
 	}
 
