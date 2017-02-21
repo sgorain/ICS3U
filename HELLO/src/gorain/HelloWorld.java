63b8d0228a8eb8322c950f20edfb976696d4fc7c
Snehan <334752052@C-3ZWJYR1.tdsb.on.ca>
2017-02-21 14:45:00 -0500

    Learning to push

---
+++ HELLO/src/gorain/HelloWorld.java
@@ -0,0 +1,16 @@
+package gorain;
+/**
+ * Hello Word
+ * Program
+ * @author Snehan
+ * February 21
+ */
+public class HelloWorld {
+static Scanner 
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!!");
+		
+
+	}
+
+}
